52c9fce9ec3a2a680a5aea1f1a7c0d33b7cfd1aa
berlinen <1292393108@qq.com>
2024-05-10 15:16:34 +0800

    feat: switch screen

---
+++ src/layout.rs
@@ -0,0 +1,107 @@
+use iced::keyboard;
+use iced::widget::{button, checkbox, column, container, horizontal_space, pick_list, row, text};
+use iced::{color, Alignment, Element, Font, Length, Subscription, Theme};
+
+use crate::views::Views;
+
+#[derive(Default, Debug)]
+pub struct Layout {
+    pub views: Views,
+    pub explain: bool,
+    pub theme: Theme,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    Next,
+    Previous,
+    ExplainToggked(bool),
+    ThemeSelected(Theme),
+}
+
+impl Layout {
+    pub fn title(&self) -> String {
+        format!("{} - Layout - Iced", self.views.title)
+    }
+
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::Next => {
+                self.views = self.views.next();
+            }
+            Message::Previous => {
+                self.views = self.views.previous();
+            }
+            Message::ExplainToggked(explain) => {
+                self.explain = explain;
+            }
+            Message::ThemeSelected(theme) => {
+                self.theme = theme;
+            }
+        }
+    }
+
+    pub fn subscription(&self) -> Subscription<Message> {
+        use keyboard::key;
+
+        keyboard::on_key_press(|key, _modifiers| match key {
+            keyboard::Key::Named(key::Named::ArrowLeft) => Some(Message::Previous),
+            keyboard::Key::Named(key::Named::ArrowRight) => Some(Message::Next),
+            _ => None,
+        })
+    }
+
+    pub fn view(&self) -> Element<Message> {
+        let header = row![
+            text(self.views.title).size(20).font(Font::MONOSPACE),
+            horizontal_space(),
+            checkbox("Explain", self.explain).on_toggle(Message::ExplainToggked),
+            pick_list(Theme::ALL, Some(&self.theme), Message::ThemeSelected),
+        ]
+        .spacing(20)
+        .align_items(Alignment::Center);
+
+        let example = container(if self.explain {
+            self.views.view().explain(color!(0x0000ff))
+        } else {
+            self.views.view()
+        })
+        .style(|theme| {
+            let palette = theme.extended_palette();
+
+            container::Style::default().with_border(palette.background.strong.color, 4.0)
+        })
+        .padding(4)
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .center_x()
+        .center_y();
+
+        let controls = row([
+            (!self.views.is_first()).then_some(
+                button("← Previous")
+                    .padding([5, 10])
+                    .on_press(Message::Previous)
+                    .into(),
+            ),
+            Some(horizontal_space().into()),
+            (!self.views.is_last()).then_some(
+                button("Next →")
+                    .padding([5, 10])
+                    .on_press(Message::Next)
+                    .into(),
+            ),
+        ]
+        .into_iter()
+        .flatten());
+
+        column![header, example, controls]
+            .spacing(10)
+            .padding(20)
+            .into()
+    }
+
+    pub fn theme(&self) -> Theme {
+        self.theme.clone()
+    }
+}
---
+++ src/views.rs
@@ -0,0 +1,134 @@
+use crate::Message;
+use iced::mouse;
+use iced::widget::{canvas, column, container, horizontal_space, row, scrollable};
+use iced::{Alignment, Element, Length, Point, Rectangle, Renderer, Theme};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Views {
+    pub title: &'static str,
+    pub view: fn() -> Element<'static, Message>,
+}
+
+impl Views {
+    const LIST: &'static [Self] = &[
+        Self {
+            title: "园区SHE",
+            view: application,
+        },
+        Self {
+            title: "园区SHE2",
+            view: application,
+        },
+    ];
+
+    pub fn is_first(self) -> bool {
+        Self::LIST.first() == Some(&self)
+    }
+
+    pub fn is_last(self) -> bool {
+        Self::LIST.last() == Some(&self)
+    }
+
+    pub fn previous(self) -> Self {
+        let Some(index) = Self::LIST.iter().position(|&example| example == self) else {
+            return self;
+        };
+
+        Self::LIST
+            .get(index.saturating_sub(1))
+            .copied()
+            .unwrap_or(self)
+    }
+
+    pub fn next(self) -> Self {
+        let Some(index) = Self::LIST.iter().position(|&example| example == self) else {
+            return self;
+        };
+
+        Self::LIST.get(index + 1).copied().unwrap_or(self)
+    }
+
+    pub fn view(&self) -> Element<Message> {
+        (self.view)()
+    }
+}
+
+impl Default for Views {
+    fn default() -> Self {
+        Self::LIST[0]
+    }
+}
+
+fn application<'a>() -> Element<'a, Message> {
+    let header = container(
+        row![
+            square(40),
+            horizontal_space(),
+            "Header!",
+            horizontal_space(),
+            square(40),
+        ]
+        .padding(10)
+        .align_items(Alignment::Center),
+    )
+    .style(|theme| {
+        let palette = theme.extended_palette();
+
+        container::Style::default().with_border(palette.background.strong.color, 1)
+    });
+
+    let sidebar = container(
+        column!["Sidebar!", square(50), square(50)]
+            .spacing(40)
+            .padding(10)
+            .width(200)
+            .align_items(Alignment::Center),
+    )
+    .style(container::rounded_box)
+    .height(Length::Fill)
+    .center_y();
+
+    let content = container(
+        scrollable(
+            column!["Content!", square(400), square(200), square(400), "The end"]
+                .spacing(40)
+                .align_items(Alignment::Center)
+                .width(Length::Fill),
+        )
+        .height(Length::Fill),
+    )
+    .padding(10);
+
+    column![header, row![sidebar, content]].into()
+}
+
+fn square<'a>(size: impl Into<Length> + Copy) -> Element<'a, Message> {
+    struct Square;
+
+    impl canvas::Program<Message> for Square {
+        type State = ();
+
+        fn draw(
+            &self,
+            _state: &Self::State,
+            renderer: &Renderer,
+            theme: &Theme,
+            bounds: Rectangle,
+            _cursor: mouse::Cursor,
+        ) -> Vec<canvas::Geometry> {
+            let mut frame = canvas::Frame::new(renderer, bounds.size());
+
+            let palette = theme.extended_palette();
+
+            frame.fill_rectangle(
+                Point::ORIGIN,
+                bounds.size(),
+                palette.background.strong.color,
+            );
+
+            vec![frame.into_geometry()]
+        }
+    }
+
+    canvas(Square).width(size).height(size).into()
+}
